602bf1e5d7de374959af6d30fa1121e59a4d44df
Maciej Dybala <maciej.dybala@gmail.com>
2022-07-25 07:38:40 +0100

    Add commands from 'Creating Functions'

--- script.R
+++ script.R
@@ -1,3 +1,7 @@
+#=================================
+#  Analyzing Patient Data
+#=================================
+
 dat <- read.csv(file = "data/inflammation-01.csv", header = FALSE)
 head(dat)
 class(dat)
@@ -74,3 +78,198 @@ plot(min_day_inflammation)
 sd_day_inflammation <- apply(dat,2,sd)
 plot(sd_day_inflammation)
 
+#=================================
+#  Creating Functions
+#=================================
+
+fahrenheit_to_celsius <- function(temp_F) {
+  temp_C <- (temp_F - 32) * 5 / 9
+  return(temp_C)
+}
+
+#Freezing point of water
+fahrenheit_to_celsius(32)
+
+# Boiling point of water
+fahrenheit_to_celsius(212)
+
+celsius_to_kelvin <- function(temp_C) {
+  temp_K <- temp_C + 273.15
+  return(temp_K)
+}
+
+# freezing point of water in Kelvin
+celsius_to_kelvin(0)
+
+fahrenheit_to_kelvin <- function(temp_F) {
+  temp_C <- fahrenheit_to_celsius(temp_F)
+  temp_K <- celsius_to_kelvin(temp_C)
+  return(temp_K)
+}
+
+# freezing point of water in Kelvin
+fahrenheit_to_kelvin(32.0)
+
+x <- c("A","B","C")
+
+highlight_content <- function(content, wrapper) {
+  highlight <- c(wrapper, content, wrapper)
+  return(highlight)
+}
+
+highlight_content(best_practice, asterisk)
+
+best_practice <- c("Write", "programs", "for", "people", "not", "computers")
+asterisk <- "***" # R interprets a variable with a single value as a vector
+                  # with one element.
+highlight(best_practice, asterisk)
+
+edges <- function(v) {
+  first <- v[1]
+  last <- v[length(v)]
+  answer <- c(first, last)
+  return(answer)
+}
+
+dry_principle <- c("Don't", "repeat", "yourself", "or", "others")
+edges(dry_principle)
+
+center <- function(data, midpoint) {
+  new_data <- (data - mean(data)) + midpoint 
+  return(new_data)
+}
+
+z <- c(0,0,0,0)
+z
+
+center(z,3)
+
+dat <- read.csv(file = "data/inflammation-01.csv", header = FALSE)
+centered <- center(dat[,4],0)
+(centered)
+
+# original mean
+mean(dat[, 4])
+
+# centered mean
+mean(centered)
+
+# original standard deviation
+sd(dat[,4])
+sd(centered)
+sd(dat[,4]) - sd(centered)
+all.equal(sd(dat[,4]), sd(centered))
+
+# new data object and set one value in column 4 to NA
+datNA <- dat
+datNA[10,4] <- NA 
+
+# returns all NA values
+center(datNA[,4],0)
+
+center <- function(data, midpoint) {
+  # return a new vector containing the original data centered around the 
+  # midpoint.
+  # Example: center(c(1, 2, 3), 0) => c(-1, 0, 1)
+  new_data <- (data - mean(data, na.rm=TRUE)) + midpoint 
+  return(new_data)
+}
+
+center(datNA[,4], 0)
+
+datNA[,1] <- as.factor(datNA[,1])
+datNA[,2] <- as.character(datNA[,2])
+
+center(datNA[,2], 0)
+
+?read.csv
+
+analyze <- function(filename) {
+  # return graphs of average, min and max inflammation over time.
+  # Input is character string of a csv file (so it has to be in "quotation 
+  # marks")
+  dat <- read.csv(file = filename, header = FALSE)
+  avg_day_inflammation <- apply(dat, 2, mean)
+  plot(avg_day_inflammation)
+  
+  min_day_inflammation <- apply(dat, 2, min)
+  plot(min_day_inflammation)
+    
+  max_day_inflammation <- apply(dat, 2, max)
+  plot(max_day_inflammation)
+}
+
+analyze("data/inflammation-03.csv")
+
+dat <- read.csv(file = "data/inflammation-02.csv", header = FALSE)
+dat
+
+rescale <- function(vector) {
+  # re scales the input vector so that the values lie between 0 and 1.
+  # The replacement for a value v is (v-L)/(H-L), where H and L are the
+  # highest and lowest values of the vector, respectively.
+  H <- max(vector)
+  L <- min(vector)
+  vector <- (vector-L) / (H - L)
+  return(vector)
+}
+a <- c(2,3,4,5,6,7,8,9,10)
+rescale(a)
+plot(rescale(a))
+
+dat <- read.csv("data/inflammation-01.csv", FALSE)
+dat
+dat <- read.csv(header = FALSE, file = "data/inflammation-01.csv")
+dat
+dat <- read.csv(FALSE, "data/inflammation-01.csv")
+
+center <- function(data, midpoint = 0) {
+  # return a new vector containing the original data centred around the 
+  # midpoint (0 by default).
+  # Example: center(c(1,2,3), 0) => c(-1,0,1)
+  new_data <- (data - mean(data)) + midpoint 
+  return(new_data)
+}
+
+test_data <- c(0,0,0,0)
+center(test_data,3)
+more_data <- 5 + test_data
+more_data
+center(more_data)
+
+display <- function(a = 1, b = 2, c = 3) {
+  result <- c(a, b, c)
+  names(result) <- c("a", "b", "c") # This names each element of the vector
+  return(result)
+}
+
+# no arguments
+display()
+
+# one argument
+display(55)
+
+# two arguments
+display(55, 66)
+
+# three arguments
+display(55, 66, 77)
+
+# only setting the value of c
+display(c = 77)
+
+?read.csv
+
+rescale <- function(vector, lower = 0, upper = 1) {
+  # re scales the input vector so that the values lie between 0 and 1.
+  # The replacement for a value v is (v-L)/(H-L), where H and L are the
+  # highest and lowest values of the vector, respectively.
+  H <- max(vector)
+  L <- min(vector)
+  vector <- (vector-L) / (H - L) * (upper - lower) + lower
+  return(vector)
+}
+
+test <- c(2, 3, 6, 4, 1, 0, 8, 9)
+rescale(test)
+rescale(test, 2, 10)
