897a354afb737e4a7fb541a6196f9c19a44be3cb
Maciej Dybala <maciej.dybala@gmail.com>
2022-08-02 15:41:11 +0100

    Add 'Data Types and Structures'.

--- code/script.R
+++ code/script.R
@@ -862,3 +862,181 @@ levels(dat$Gender)[2] <- 'f'
 
 plot(x = dat$Gender, y = dat$BloodPressure)
 
+#===============================
+# Data Types and Structures
+#===============================
+
+# Example
+x <- "dataset"
+typeof(x)
+
+attributes(x)
+
+y <- 1:10
+y
+typeof(y)
+length(y)
+
+z <- as.numeric(y)
+z
+typeof(z)
+
+?typeof()
+
+vector() # an empty 'logical' (the default) vector
+
+vector("character", length = 5) # a vector of mode 'character' with 5 elements
+
+character(5) # the same thing, but using the constructor directly
+
+numeric(5) # a numeric vector with 5 elements
+
+logical(5) # a logical vector with 5 elements
+
+x <- c(1, 2, 3)
+
+x1 <- c(1L, 2L, 3L)
+
+x1
+
+y <- c(TRUE, TRUE, FALSE, FALSE)
+
+y
+
+z <- c("Sarah", "Tracy", "Jon")
+
+z
+
+typeof(z)
+
+length(z)
+
+class(z)
+
+str(z)
+
+z <- c(z, "Annette")
+
+z
+
+z <- c("Greg", z)
+
+z
+
+series <- 1:10
+seq(series)
+seq(10)
+seq(from = 1, to = 10, by = 0.1)
+
+x <- c(0.5, NA, 0.7)
+x
+
+x <- (TRUE, FALSE, NA)
+x
+
+x <- c("a", NA, "c", "d", "e")
+x
+
+x <- c(1+5i, 2-3i, NA)
+x
+
+x <- c("a", NA, "c", "d", NA)
+y <- c("a", "b", "c", "d", "e")
+is.na(x)
+is.na(y)
+anyNA(x)
+anyNA(y)
+
+1/0
+0/0
+
+xx <- c(1.7, "a")
+class(xx)
+
+xx <- c(TRUE, 2)
+class(xx)
+
+xx <- c("a", TRUE)
+class(xx)
+
+as.numeric("1")
+as.character(1:2)
+
+length(1:10)
+nchar("Software Carpentry")
+
+m <- matrix(nrow = 2, ncol = 2)
+m
+dim(m)
+
+m <- matrix(c(1:3))
+class(m)
+typeof(m)
+
+FOURS <- matrix(
+  c(4, 4, 4, 4),
+  nrow = 2,
+  ncol = 2)
+
+typeof(FOURS[1])
+
+m <- matrix(1:6, nrow = 2, ncol = 3)
+m
+
+m <- 1:10
+dim(m) <- c(2, 5)
+m
+
+x <- 1:3
+y <- 10:12
+cbind(x, y)
+rbind(x, y)
+
+mdat <- matrix(c(1, 2, 3, 11, 12, 13),
+               nrow = 2,
+               ncol = 3,
+               byrow = TRUE)
+mdat
+mdat[2,3]
+
+x <- list(1, "a", TRUE, 1+4i)
+x
+
+x <- vector("list", length = 5) # empty list
+length(x)
+
+x[[1]]
+
+x <- 1:10
+x <- as.list(x)
+length(x)
+class(x[1])
+class(x[[1]])
+
+xlist <- list(a = "Karthik Ram", b = 1:10, data = head(mtcars))
+xlist
+
+names(xlist)
+
+length(xlist)
+str(xlist)
+xlist
+xlist$data
+
+dat <- data.frame(id = letters[1:10], x = 1:10, y = 11:20)
+dat
+head(dat)
+tail(dat)
+dim(dat)
+nrow(dat)
+ncol(dat)
+str(dat)
+names(dat)
+colnames(dat)
+sapply(dat, class)
+is.list(dat)
+class(dat)
+dat[1, 3]
+dat[["y"]]
+dat$y
+str(PlantGrowth)
